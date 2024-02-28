169d9892b9cbe1e44214724bc4e90393d3cff470
okoid721 <okoid721@gmail.com>
2024-02-28 01:12:26 +0100

    e-commerce

--- phone-eccomerce/app/components/products/ProductCard.tsx
+++ phone-eccomerce/app/components/products/ProductCard.tsx
@@ -1,5 +1,21 @@
+'use client';
+
 const ProductCard = () => {
-  return <div></div>;
+  return (
+    <div className=" col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm ">
+      <div
+        className=" flex
+       flex-col 
+        items-center w-full gap-1"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
