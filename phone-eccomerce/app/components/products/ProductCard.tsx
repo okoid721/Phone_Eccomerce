1dc80be09dbf55623ea7dbd550688f18d8f21cb2
okoid721 <okoid721@gmail.com>
2024-02-28 13:10:24 +0100

    e-commerce

--- phone-eccomerce/app/components/products/ProductCard.tsx
+++ phone-eccomerce/app/components/products/ProductCard.tsx
@@ -1,31 +1,30 @@
 'use client';
 
+import { formatPrice } from '@/utils/formatPrice';
+import { truncateText } from '@/utils/truncateText';
 import Image from 'next/image';
+import React from 'react';
 
-interface ProductCardProps {
+interface ProductCradProps {
   data: any;
 }
-
-const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+const ProductCard: React.FC<ProductCradProps> = ({ data }) => {
   return (
-    <div className=" col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm ">
-      <div
-        className=" flex
-       flex-col 
-        items-center w-full gap-1"
-      >
-        <div className=" aspect-square overflow-hidden relative w-full ">
+    <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
+      <div className="flex flex-col items-center w-full gap-1">
+        <div className=" aspect-square overflow-hidden relative w-full">
           <Image
             fill
-            src={data.images[0]}
+            className="
+            w-full h-full object-contain"
+            src={data.images[0].image}
             alt={data.name}
-            className=" w-full h-full object-contain"
           />
         </div>
+        <div className="mt-4">{truncateText(data.name)}</div>
         <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>{data.reviews.length} reviews</div>
+        <div>{formatPrice(data.price)}</div>
       </div>
     </div>
   );
