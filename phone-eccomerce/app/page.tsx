fc17625476ced640846d392756de3a1a3481bc61
okoid721 <okoid721@gmail.com>
2024-02-28 12:53:00 +0100

    e-commerce

--- phone-eccomerce/app/page.tsx
+++ phone-eccomerce/app/page.tsx
@@ -13,14 +13,9 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
-            return (
-              <div key={product.id}>
-                {' '}
-                <ProductCard data={product} />{' '}
-              </div>
-            );
+            return <ProductCard data={product} />;
           })}
         </div>
       </Containers>
--- phone-eccomerce/next.config.js
+++ phone-eccomerce/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      },
-    ],
-    minimumCacheTTL: 15000000,
+    domains: ['m.media-amazon.com'],
   },
 };
 
