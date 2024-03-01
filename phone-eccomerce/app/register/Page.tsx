4a9116b91f37290f000ac85b5733154c0248ddf9
okoid721 <okoid721@gmail.com>
2024-03-01 15:46:36 +0100

    eccomerce

---
+++ phone-eccomerce/app/register/Page.tsx
@@ -0,0 +1,15 @@
+import Containers from '../components/Containers';
+import FormWrap from '../components/FormWrap';
+import RegisterFrom from './RegisterFrom';
+
+const Register = () => {
+  return (
+    <Containers>
+      <FormWrap>
+        <RegisterFrom />
+      </FormWrap>
+    </Containers>
+  );
+};
+
+export default Register;
---
+++ phone-eccomerce/app/register/RegisterFrom.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Haeding from '../components/Haeding';
+
+const RegisterFrom = () => {
+  return (
+    <>
+      <Haeding title="Sign Up For Kings~Mobile" />
+      <hr className=" bg-slate-500 w-full h-px" />
+    </>
+  );
+};
+
+export default RegisterFrom;
