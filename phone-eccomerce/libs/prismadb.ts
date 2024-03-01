b0d95ccf4b383d204f155ad127a5cd364fc87814
okoid721 <okoid721@gmail.com>
2024-03-01 19:23:04 +0100

    eccomerce

--- phone-eccomerce/libs/prismadb.ts
+++ phone-eccomerce/libs/prismadb.ts
@@ -4,7 +4,7 @@ declare global {
     var prisma:PrismaClient | undefined
 }
 
-const client = globalThis.prisma || PrismaClient()
+const client = globalThis.prisma || new PrismaClient()
 
 
 
--- phone-eccomerce/pages/api/auth/[...nextauth].ts
+++ phone-eccomerce/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import Prisma from "@/libs/prismadb"
+import prisma from "@/libs/prismadb"
 import bcrypt from 'bcrypt'
 
 
