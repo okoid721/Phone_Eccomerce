c1a2318d6407adb67904d163b6b2ab5d760d1dc5
okoid721 <okoid721@gmail.com>
2024-03-01 18:33:58 +0100

    eccomerce

--- phone-eccomerce/pages/api/auth/[...nextauth].ts
+++ phone-eccomerce/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import Prisma from "@/libs/prismadb"
+import bcrypt from 'bcrypt'
 
 
 
@@ -19,5 +21,40 @@ export default NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
+        CredentialProvider({
+            name: "credentials",
+            credentials: {
+            email: {
+                label: 'email',
+                type: 'text',
+            },
+            password: {
+                label: "password",
+                type: "password"
+            }
+        },
+        async authorize(credentials){
+            if(!credentials?.email ||  !credentials.password){
+                throw new Error('Email or Password is required')
+            }
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: credentials.email
+                }
+            })
+            if(!user || !user?.hashedPassword){
+                throw new Error('invalid User')
+            }
+
+            const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+            
+            if (!isCorrectPassword) {
+              throw new Error("Incorrect password")
+            }
+          
+            return user;
+        }
+        })
     ]
 })
