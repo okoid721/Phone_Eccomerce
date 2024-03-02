a0d736ddeb54e206579cbeba6830ff6a0db9ae3d
okoid721 <okoid721@gmail.com>
2024-03-02 01:38:39 +0100

    eccomerce

--- phone-eccomerce/app/components/navbar/UserMenu.tsx
+++ phone-eccomerce/app/components/navbar/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { useCallback, useState } from 'react';
 import Avatar from '../Avatar';
 import { AiFillCaretDown } from 'react-icons/ai';
+import Link from 'next/link';
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,11 +15,18 @@ const UserMenu = () => {
       <div className=" relative z-30">
         <div
           onClick={toggleOpen}
-          className=" p-2 border-[1px] border-slate-500 flex flex-row items-center gap-1  "
+          className=" p-2 border-[1px] border-slate-500 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700 "
         >
           <Avatar />
           <AiFillCaretDown />
         </div>
+        {isOpen && (
+          <div className="absolute rounded-md shadow-md w-170px bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer ">
+            <div>
+              <Link href={'orders'}></Link>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
