32ca7382d8a4c30b2e05fb3be67eb76df14e6ad3
okoid721 <okoid721@gmail.com>
2024-03-09 00:43:19 +0100

    eccomerce

--- phone-eccomerce/app/admin/manage-product/page.tsx
+++ phone-eccomerce/app/admin/manage-product/page.tsx
@@ -1,5 +1,26 @@
-const ManageProduct = async () => {
-  return <div>add product</div>;
+import React from 'react';
+
+import getProducts from '@/actions/getProduct';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import NullData from '@/app/components/NullData';
+import ManageProductsClient from './ManageProuctClient';
+import Containers from '@/app/components/Containers';
+
+const ManageProducts = async () => {
+  const products = await getProducts({ category: null });
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title="Oops! Access Denied" />;
+  }
+
+  return (
+    <div className="p-8 bg-white text-[#0F1111] select-none ">
+      <Containers>
+        <ManageProductsClient products={products} />
+      </Containers>
+    </div>
+  );
 };
 
-export default ManageProduct;
+export default ManageProducts;
