3fb660d7c78724dadef9818a6aeca966318028e7
okoid721 <okoid721@gmail.com>
2024-03-08 01:08:54 +0100

    eccomerce

--- phone-eccomerce/app/admin/add-product/AddProductForm.tsx
+++ phone-eccomerce/app/admin/add-product/AddProductForm.tsx
@@ -1,13 +1,16 @@
 'use client';
 import Haeding from '@/app/components/Haeding';
+import CategoryInput from '@/app/components/inputs/CategoryInput';
 import CustomCheckBox from '@/app/components/inputs/CustomCheckBox';
 import Input from '@/app/components/inputs/Input';
 import TextArea from '@/app/components/inputs/TextArea';
+import { categories } from '@/utils/Categorise';
 import React, { useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 
 const AddProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -26,6 +29,16 @@ const AddProductForm = () => {
       price: '',
     },
   });
+
+  const category = watch('category');
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
   return (
     <>
       <Haeding title="Add A Product" />
@@ -69,7 +82,23 @@ const AddProductForm = () => {
       />
       <div className="w-full font-medium ">
         <div className=" mb-2 font-bold "></div>
-        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto "></div>
+        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto ">
+          {categories.map((item) => {
+            if (item.label === 'ALL') {
+              return null;
+            }
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue('category', category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
