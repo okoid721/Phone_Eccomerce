655ae6b800ff8cda104429e3276d737bc03b922c
okoid721 <okoid721@gmail.com>
2024-03-09 14:54:10 +0100

    eccomerce

--- phone-eccomerce/app/admin/manage-order/page.tsx
+++ phone-eccomerce/app/admin/manage-order/page.tsx
@@ -1,5 +1,25 @@
-const ManageOrder = async () => {
-  return <div>add product</div>;
+import React from 'react';
+import ManageOrdersClient from './ManageOrdersClient';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import NullData from '@/app/components/NullData';
+import getOrders from '@/actions/getOrders';
+import Containers from '@/app/components/Containers';
+
+const ManageOrders = async () => {
+  const orders = await getOrders();
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title="Oops! Access Denied" />;
+  }
+
+  return (
+    <div className="p-8 bg-white text-[#0F1111] select-none ">
+      <Containers>
+        <ManageOrdersClient orders={orders} />
+      </Containers>
+    </div>
+  );
 };
 
-export default ManageOrder;
+export default ManageOrders;
