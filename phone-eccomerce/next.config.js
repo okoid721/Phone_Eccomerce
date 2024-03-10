275c97af57b04bdf7ce54a4c2aaf8e642e1fb1a2
okoid721 <okoid721@gmail.com>
2024-03-10 12:22:12 +0100

    eccomerce

--- phone-eccomerce/next.config.js
+++ phone-eccomerce/next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['m.media-amazon.com', 'lh3.googleusercontent.com'],
-  },
-};
-module.exports = {
-  // ...
-  async rewrites() {
-    return [
-      {
-        source: '/api/paystack/callback',
-        destination: '/api/paystack/callback', // Your actual API route path
-      },
-    ];
+    domains: [
+      'm.media-amazon.com',
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+    ],
   },
 };
 
