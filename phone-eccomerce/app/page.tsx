ccd1581ee65de5c39806618fb0ff7b4d326a643b
okoid721 <okoid721@gmail.com>
2024-03-10 12:14:00 +0100

    eccomerce

--- phone-eccomerce/app/page.tsx
+++ phone-eccomerce/app/page.tsx
@@ -1,21 +1,46 @@
-/* eslint-disable react/jsx-key */
-import { product } from '@/utils/product';
-import Containers from './components/Containers';
-import HomeBanner from './components/HomeBanner';
-import { truncate } from 'fs';
-import { truncateText } from '@/utils/truncateText';
-import ProductCard from './components/products/ProductCard';
+export const revalidate = 0;
 
-export default function Home() {
+import HomeBanner from './components/HomeBanner';
+import ProductCard from './components/products/ProductCard';
+import getProducts, { IProductParams } from '@/actions/getProduct';
+import React from 'react';
+import NullData from './components/NullData';
+import Containers from './components/Containers';
+
+interface HomeProps {
+  searchParams: IProductParams;
+}
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
+
+  if (products.length === 0) {
+    return (
+      <NullData title="OOps! No products found. Click on category All to clean filters" />
+    );
+  }
+
+  // Fisher-Yates shuffle algorithm
+
+  function shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+  }
+
+  const shuffledProducts = shuffleArray(products);
+
   return (
-    <div className=" p-8">
+    <div className="p-8 text-[#0F1111] select-none">
       <Containers>
         <div>
           <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {product.map((product: any) => {
-            return <ProductCard data={product} />;
+          {shuffledProducts.map((product: any) => {
+            return <ProductCard data={product} key={product._id} />;
           })}
         </div>
       </Containers>
