9c0fd8cf68ed4ad32858c2ac7c7f4162270d1959
okoid721 <okoid721@gmail.com>
2024-03-12 00:43:47 +0100

    eccomerce

--- phone-eccomerce/app/admin/BarGraph.tsx
+++ phone-eccomerce/app/admin/BarGraph.tsx
@@ -1,55 +1,55 @@
-// "use client";
-// import React from "react";
+'use client';
+import React from 'react';
 
-// import {
-//   Chart as ChartJS,
-//   BarElement,
-//   CategoryScale,
-//   Tooltip,
-//   LinearScale,
-//   Legend,
-// } from "chart.js";
-// import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  Tooltip,
+  LinearScale,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-// ChartJS.register(BarElement, CategoryScale, Tooltip, LinearScale, Legend);
+ChartJS.register(BarElement, CategoryScale, Tooltip, LinearScale, Legend);
 
-// interface BarGraphProps {
-//   data: GraphData[];
-// }
+interface BarGraphProps {
+  data: GraphData[];
+}
 
-// type GraphData = {
-//   day: string;
-//   date: string;
-//   totalAmount: number;
-// };
-// const BarGraph: React.FC<BarGraphProps> = ({ data }) => {
-//   const labels = data.map((item) => item.day);
-//   const amounts = data.map((item) => item.totalAmount / 100);
+type GraphData = {
+  day: string;
+  date: string;
+  totalAmount: number;
+};
+const BarGraph: React.FC<BarGraphProps> = ({ data }) => {
+  const labels = data.map((item) => item.day);
+  const amounts = data.map((item) => item.totalAmount / 100);
 
-//   const chartData = {
-//     labels: labels,
-//     datasets: [
-//       {
-//         label: "Your King Mobile Sale Amount",
-//         data: amounts,
-//         backgroundColor: "#F9A024",
-//         borderWidth: 1,
-//       },
-//     ],
-//   };
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Your King Mobile Sale Amount',
+        data: amounts,
+        backgroundColor: '#F9A024',
+        borderWidth: 1,
+      },
+    ],
+  };
 
-//   const options = {
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   };
-//   return (
-//     <div className="h-[70vh]">
-//       <Bar data={chartData} options={options}></Bar>
-//     </div>
-//   );
-// };
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+  return (
+    <div className="h-[70vh]">
+      <Bar data={chartData} options={options}></Bar>
+    </div>
+  );
+};
 
-// export default BarGraph;
+export default BarGraph;
